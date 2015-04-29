219ab8c5dd6bc99ada52d5279f2902d5d670e6bd
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-04-29 19:37:46 +0900

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :edit, :update]
+  #patch 'user/:id' => 'user#new', as: :user
+  #get 'user/:id' => 'user#new', as: :user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
